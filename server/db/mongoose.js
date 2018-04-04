53f7fd4dbbdcd6086a774e9b4960450a5b35e670
sounak07 <sounakume@gmail.com>
2018-04-04 20:56:46 +0530

    Get by id added

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require("mongoose");
 
+//to use Promise
 mongoose.Promise = global.Promise;
+
+//connection establised
 mongoose.connect("mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose};
--- server/server.js
+++ server/server.js
@@ -5,6 +5,7 @@ var bodyParser = require("body-parser");
 var {mongoose} = require("./db/mongoose");
 var {Todo} = require("./models/todo");
 var {User} = require("./models/user");
+var {ObjectID} = require("mongodb");
 
 var app = express();
 
@@ -25,12 +26,33 @@ app.post("/todos",(req,res) => {
 
 });
 
+//GET route
 app.get("/todos",(req,res)=>{
   Todo.find().then((todos) => {
     res.send({todos});
   },(e)=>{
     res.status(400).send(e);
   });
+});
+
+//GET /todos/125646
+app.get("/todos/:id",(req, res)=>{
+  var id = req.params.id;
+
+  if(!ObjectID.isValid(id)){
+      return res.status(404).send();
+  }
+
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      res.status(404).send();
+    }
+
+     res.send({todo});
+  }).catch((e)=>{
+    res,status(400).send();
+  });
+
 });
 
 app.listen(3000,()=>{
