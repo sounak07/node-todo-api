cc39c5667d2aac5673a04ffcb8d98547062b97d6
sounak07 <sounakume@gmail.com>
2018-04-15 22:06:50 +0530

    removed useMongoClient from code

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -6,6 +6,6 @@ mongoose.Promise = global.Promise;
 //connection establised
 // mongoose.connect("mongodb://localhost:27017/TodoApp");
 var url = process.env.databaseURL || "mongodb://localhost:27017/TodoApp";
-mongoose.connect(url,{useMongoClient: true});
+mongoose.connect(url);
 
 module.exports = {mongoose};
