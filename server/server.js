cd87fde2ccb74175a2f126dc43d22d3af67a35c0
sounak07 <sounakume@gmail.com>
2018-04-17 20:37:43 +0530

    Delete test added

--- server/server.js
+++ server/server.js
@@ -68,7 +68,7 @@ app.delete("/todos/:id",(req,res)=>{
       if(!todo){
         res.status(404).send();
       } else {
-        res.status(200).send(todo);
+        res.status(200).send({todo});
       }
 
   }).catch((e)=>{
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -5,13 +5,13 @@ var {ObjectID} = require("mongodb");
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
-// const todos = [{
-//   _id:new ObjectID(),
-//   text: 'First test todo'
-// }, {
-//   _id:new ObjectID(),
-//   text: 'Second test todo'
-// }];
+const todos = [{
+  _id:new ObjectID(),
+  text: 'First test todo'
+}, {
+  _id:new ObjectID(),
+  text: 'Second test todo'
+}];
 
 beforeEach(function(done){
   this.timeout(3000);
@@ -100,6 +100,47 @@ describe("GET/todos/:id",()=>{
       .end(done);
   });
 });
+
+
+describe("DELETE /todos/:id",()=>{
+  it("should remove a todo",(done)=>{
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${hexId}`)
+      .expect(200)
+      .expect((res)=>{
+        expect(res.body.todo._id).toBe(hexId);
+      })
+      .end((err, res)=>{
+        if(err){
+          return done(err);
+        }
+
+        Todo.findById(hexId).then((todo)=>{
+          expect(todo).toBeFalsy();
+          done();
+        }).catch((e) => done(e));
+      });
+
+  });
+
+  it("should return 404 if todo not found",(done)=>{
+    var hexId = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexId}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 404 if todo id is invalid",(done)=>{
+
+    request(app)
+      .get(`/todos/1212`)
+      .expect(404)
+      .end(done);
+  });
+})
 
 
 // beforeEach(async () => {
