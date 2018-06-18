7ed74980d0166f24456015e57491b0d71b614a5f
sounak07 <sounakume@gmail.com>
2018-06-18 21:32:00 +0530

    Auth using jwt added

--- server/models/user.js
+++ server/models/user.js
@@ -53,6 +53,16 @@ UserSchema.methods.generateAuthToken = function(){
   });
 };
 
+UserSchema.methods.removeToken = function(token){
+  var user = this;
+
+  return user.update({
+    $pull: {
+      tokens:{token}
+    }
+  })
+};
+
 UserSchema.statics.findByToken = function(token){
   var User = this;
   var decoded;
@@ -70,6 +80,26 @@ UserSchema.statics.findByToken = function(token){
   });
 };
 
+UserSchema.statics.findByCredentials = function(email, password){
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if(!user){
+      return Promise.reject()
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare( password, user.password, (err, res) => {
+        if(res){
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+  });
+};
+
 UserSchema.pre("save",function(next){
   var user = this;
 
@@ -81,7 +111,7 @@ UserSchema.pre("save",function(next){
       });
     });
   } else {
-    next();
+    return next();
   }
 });
 
